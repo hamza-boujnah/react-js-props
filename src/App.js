ab200411fa32f80c3881d1213f4cd22039282316
hamza boujnah <hamzus.2727@gmail.com>
2023-10-26 14:28:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PlayersList from './PlayersList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIFA Player Cards</h1>
+      <PlayersList />
+    </div>
+  );
+}
+export default App;
---
+++ src/players.js
@@ -0,0 +1,41 @@
+// src/players.js
+// i have a problem with the image of the players 
+const players = [
+    {
+      name: 'Saad Bguir',
+      team: 'Esperance Sportive de Tunis',
+      nationality: 'Tunisian',
+      jerseyNumber: 10,
+      age: 27,
+      imageURL: '/assets/saad.jpg',
+    },
+    {
+      name: 'Cristiano Ronaldo',
+      team: 'Manchester United',
+      nationality: 'Portuguese',
+      jerseyNumber: 7,
+      age: 36,
+      imageURL: '/assets/cr.jpg',
+    },
+    {
+      name: 'Marcelo',
+      team: 'Real Madrid',
+      nationality: 'Brazilian',
+      jerseyNumber: 12,
+      age: 33,
+      imageURL: '/assets/marcelo.jpg',
+    },
+    { 
+      name: 'Sergio Ramos',
+      team: 'Paris Saint-Germain',
+      nationality: 'Spanish',
+      jerseyNumber: 4,
+      age: 35,
+      imageURL: '/assets/ramos.jpg',
+    },
+    // You can add more players if needed .
+    
+  ];
+  
+  export default players;
+  
